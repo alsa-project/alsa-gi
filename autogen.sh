687ff0a52d4b61d59194a0cb44f28943ad93d1b9
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-11-15 09:11:05 +0900

    all: check needed directories to build and make it if missing

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,18 @@
 set -u
 set -e
 
+dirs="card compress control hwdep pcm rawmidi seq timer"
+
 if [ ! -e m4 ] ; then
 	mkdir -p m4
 fi
+
+for i in $dirs; do
+	dir="src/${i}"
+	if [ ! -e $dir ] ; then
+		mkdir $dir
+	fi
+done
 
 gtkdocize --copy --docdir doc/reference
 autoreconf --install
