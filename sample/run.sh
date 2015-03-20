aaa25a6150ec138b5541056f6ed48a6271d20f8d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-03-20 16:28:00 +0900

    ctl: move to ctl directory

--- sample/run.sh
+++ sample/run.sh
@@ -3,13 +3,13 @@
 # NOTE:
 # Current working directory should be in root of this repository.
 
-export LD_LIBRARY_PATH=src/timer/.libs/:src/seq/.libs/:src/control/.libs/:/usr/lib:/lib
-export GI_TYPELIB_PATH=src/timer/:src/seq/:src/control/:/usr/lib/girepository-1.0
+export LD_LIBRARY_PATH=src/timer/.libs/:src/seq/.libs/:src/ctl/.libs/:/usr/lib:/lib
+export GI_TYPELIB_PATH=src/timer/:src/seq/:src/ctl/:/usr/lib/girepository-1.0
 
 if [[ $1 == 'timer' ]] ; then
 	./sample/timer.py
 elif [[ $1 == 'seq' ]] ; then
 	./sample/sequencer.py
 elif [[ $1 == 'ctl' ]] ; then
-	./sample/control.py
+	./sample/ctl.py
 fi
