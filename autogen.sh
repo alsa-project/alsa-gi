7eb1c193adfecee5c5e0d062c5d6a3e63722ad78
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-02-19 14:44:20 +0900

    autotools: generate required files for autoreconf automatically

--- autogen.sh
+++ autogen.sh
@@ -6,14 +6,20 @@ set -e
 dirs="card compress control hwdep pcm rawmidi seq timer"
 
 if [ ! -e m4 ] ; then
-	mkdir -p m4
+    mkdir -p m4
 fi
 
+for i in 'NEWS' 'ChangeLog' 'AUTHORS' ; do
+    if [ ! -e $i ] ; then
+        touch $i
+    fi
+done
+
 for i in $dirs; do
-	dir="src/${i}"
-	if [ ! -e $dir ] ; then
-		mkdir $dir
-	fi
+    dir="src/${i}"
+    if [ ! -e $dir ] ; then
+        mkdir $dir
+    fi
 done
 
 gtkdocize --copy --docdir doc/reference
